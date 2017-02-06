44925d851e3023b7bdf0e40bed59d0459bc24b13
Raul Figueira <raulfmiranda@gmail.com>
2017-02-06 10:16:10 -0300

    Layouts da tela de Login e do Validador criados.

---
+++ app/src/main/java/com/raulfmiranda/whatsapp/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.raulfmiranda.whatsapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.raulfmiranda.whatsapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
