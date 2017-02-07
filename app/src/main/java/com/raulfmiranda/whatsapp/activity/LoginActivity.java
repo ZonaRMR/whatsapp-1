0bc873c980a8e8c55ac08dd493045e44d22c8717
Raul Figueira <raulfmiranda@gmail.com>
2017-02-06 21:24:03 -0300

    Criação de máscara para telefone (EditText) com o uso da biblioteca MaskFormatter.

--- app/src/main/java/com/raulfmiranda/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/com/raulfmiranda/whatsapp/activity/LoginActivity.java
@@ -2,14 +2,39 @@ package com.raulfmiranda.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 import com.raulfmiranda.whatsapp.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText nome;
+    private EditText telefone;
+    private EditText codPais;
+    private EditText codArea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        nome = (EditText) findViewById(R.id.edit_nome);
+        telefone = (EditText) findViewById(R.id.edit_telefone);
+        codPais = (EditText) findViewById(R.id.edit_cod_pais);
+        codArea = (EditText) findViewById(R.id.edit_cod_area);
+
+        // Definir as máscaras
+        SimpleMaskFormatter simpleMaskCodPais = new SimpleMaskFormatter("+NN");
+        SimpleMaskFormatter simpleMaskCodArea = new SimpleMaskFormatter("NN");
+        SimpleMaskFormatter simpleMaskTelefone = new SimpleMaskFormatter("NNNNN-NNNN");
+
+        MaskTextWatcher maskCodPais = new MaskTextWatcher(codPais, simpleMaskCodPais);
+        MaskTextWatcher maskCodArea = new MaskTextWatcher(codArea, simpleMaskCodArea);
+        MaskTextWatcher maskTelefone = new MaskTextWatcher(telefone, simpleMaskTelefone);
+
+        telefone.addTextChangedListener(maskTelefone);
+        codPais.addTextChangedListener(maskCodPais);
+        codArea.addTextChangedListener(maskCodArea);
     }
 }
