1e8ce2b854f50aa79a8c6cd84f59072b18a5dc5b
Raul Figueira <raulfmiranda@gmail.com>
2017-02-09 11:35:26 -0300

    Criado cadastro de usuários com tratamento de exceções (Firebase) .

--- app/src/main/java/com/raulfmiranda/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/com/raulfmiranda/whatsapp/activity/LoginActivity.java
@@ -6,16 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
 import com.raulfmiranda.whatsapp.R;
+import com.raulfmiranda.whatsapp.config.ConfiguracaoFirebase;
 
 
 public class LoginActivity extends AppCompatActivity {
+
+    private DatabaseReference referenciaFirebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
 
     }
 
