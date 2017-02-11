2e74e1926cb1d06503191407b56d03c53dc87365
Raul Figueira <raulfmiranda@gmail.com>
2017-02-10 21:06:27 -0300

    Autenticação de login e logout (button) feitos .

--- app/src/main/java/com/raulfmiranda/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/raulfmiranda/whatsapp/activity/MainActivity.java
@@ -1,16 +1,38 @@
 package com.raulfmiranda.whatsapp.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.raulfmiranda.whatsapp.R;
+import com.raulfmiranda.whatsapp.config.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoSair;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        botaoSair = (Button) findViewById(R.id.bt_sair);
+
+        botaoSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+                autenticacao.signOut();
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
